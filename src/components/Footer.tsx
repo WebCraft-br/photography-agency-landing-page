89cd49c140d911e8320967f0a245d7a09c14df4b
bit-web24 <bitweb24@gmail.com>
2024-08-27 00:03:35 +0530

    adjust footer in main page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import Contact from './Contact';
 
 const Footer = () => {
   return (
-    <>
-      <div className="bg-[#363733] text-gray-300 py-10 rounded-2xl max-4">
+    <div className="max-4 gap-20 pb-10 px-10 mt-20">
+      <div className="bg-[#363733] text-gray-300 px-5 py-7 rounded-2xl">
         <Contact />
-        <div className="px-4 sm:px-6 lg:px-8">
+        <div className="px-5 sm:px-6 lg:px-8">
           <div className="mt-10 border-t border-gray-700 pt-6 flex flex-row items-center justify-between">
-            <img src='/logo.png' className='w-12 h-12' />
+            <img src='/logo.png' className='w-12 h-12 rounded-2xl' />
             <div className="flex space-x-6">
               <a href="#" className="hover:underline">About Us</a>
               <a href="#" className="hover:underline">Project</a>
@@ -19,7 +19,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
