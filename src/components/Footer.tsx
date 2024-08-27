a1d8c9bd23b53236abf0a5692d1c156087cbeb31
bit-web24 <bitweb24@gmail.com>
2024-08-27 21:02:54 +0530

    add logo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <Contact />
         <div className="px-4 sm:px-6 lg:px-8">
           <div className="mt-10 border-t border-gray-700 pt-6 flex flex-col sm:flex-row items-center justify-between space-y-4 sm:space-y-0 sm:justify-between">
-            <img src='/logo.png' className='w-12 h-12 rounded-2xl' alt="Logo" />
+            <div className="flex items-center space-x-2 text-white text-lg"><img src='/logo.png' className='w-12 h-12 rounded-2xl' alt="Logo" /> <p>CREACY</p> </div>
             <div className="flex flex-wrap justify-center sm:ml-10 space-x-4 sm:space-x-6">
               <a href="#" className="hover:underline">About Us</a>
               <a href="#" className="hover:underline">Project</a>
